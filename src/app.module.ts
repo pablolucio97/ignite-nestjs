6cea3fcec3cde23ccf08e9ec5a3324fe3ceb92b4
Pablo Silva <pablolucio_@hotmail.com>
2023-09-30 07:06:16 -0300

    chore: configured env vars validation schema with nest/config and zod

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
-import { PrismaService } from './prisma/prisma-service';
+import { ConfigModule } from '@nestjs/config';
 import { CreateAccountController } from './controllers/create-account-controller';
+import { envSchema } from './env';
+import { PrismaService } from './prisma/prisma-service';
 
 @Module({
+  imports: [ConfigModule.forRoot({
+    validate: env => envSchema.parse(env),
+    isGlobal: true
+  })],
   controllers: [CreateAccountController],
   providers: [PrismaService],
 })
