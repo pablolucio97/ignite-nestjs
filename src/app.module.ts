aec8a666edbeb2f892d8100e72bd8163da5819bc
Pablo Silva <pablolucio_@hotmail.com>
2023-10-05 06:28:29 -0300

    added list questions controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,22 @@ import { CreateAccountController } from './controllers/create-account-controller
 import { envSchema } from './env';
 import { PrismaService } from './prisma/prisma-service';
 import { CreateQuestionController } from './controllers/create-question.controller';
+import { FetchRecentQuestionsController } from './controllers/fetch-resent-questions-controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    validate: env => envSchema.parse(env),
-    isGlobal: true
-  }), AuthModule],
-  controllers: [CreateAccountController, AuthenticateController, CreateQuestionController],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+  ],
+  controllers: [
+    CreateAccountController,
+    AuthenticateController,
+    CreateQuestionController,
+    FetchRecentQuestionsController
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
