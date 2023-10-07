28dd2c75c5e8a3b70ec0d9400c676d6a3005fe6c
Pablo Silva <pablolucio_@hotmail.com>
2023-10-07 06:53:32 -0300

    chore: created answers migration

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -8,7 +8,7 @@ import { QuestionBestAnswerChosenEvent } from '@/domain/forum/enterprise/events/
 
 export interface QuestionProps {
   authorId: UniqueEntityID
-  bestAnswerId?: UniqueEntityID
+  bestAnswerId?: UniqueEntityID | null
   title: string
   content: string
   slug: Slug
